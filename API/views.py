db2296021c352f9e501b3eedd0891639729058da
max180643 <max180643@gmail.com>
2020-04-24 18:02:24 +0700

    :rocket: Add search API and search box

--- API/views.py
+++ API/views.py
@@ -1,23 +1,32 @@
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from Manage.models import Supplier, Stock
-from API.serializers import supplierSerializer, stockSerializer
 
+from API.serializers import supplierSerializer, stockSerializer
+from Manage.models import Supplier, Stock
 
 # Create your views here.
 
 
-# @login_required
 class api_view_supplier(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        items = Supplier.objects.all()
+        try:
+            search = request.query_params['search']
+        except:
+            search = False
+        if search:
+            items = Supplier.objects.filter(Q(name__icontains=search) | Q(
+                address__icontains=search) | Q(phone__icontains=search) | Q(email__icontains=search))
+        else:
+            items = Supplier.objects.all()
         serializer = supplierSerializer(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class api_view_stock(APIView):
     permission_classes = [IsAuthenticated]
