c0d499638fc6ba29200d9c0a432f331785902f1e
max180643 <max180643@gmail.com>
2020-04-26 02:26:24 +0700

    :rocket: Add update API for customer

--- API/serializers.py
+++ API/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from Manage.models import Supplier, Stock
+from Manage.models import Supplier, Stock, Customer
 
 
 class supplierSerializer(serializers.ModelSerializer):
@@ -27,3 +27,24 @@ class stockSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stock
         fields = ['id', 'item_id', 'color', 'amount']
+
+
+class customerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['id', 'fname', 'lname',
+                  'email', 'phone', 'address', 'account_id']
+        read_only_fields = ['id']
+        extra_kwargs = {
+            'fname': {"error_messages": {"blank": "* กรุณากรอกชื่อจริง"}},
+            'lname': {"error_messages": {"blank": "* กรุณากรอกนามสกุล"}},
+            'email': {"error_messages": {"blank": "* กรุณากรอกอีเมล", 'invalid': "* กรุณากรอกอีเมลให้ถูกต้อง"}},
+            'phone': {"error_messages": {"blank": "* กรุณากรอกเบอร์มือถือ"}},
+            'address': {"error_messages": {"blank": "* กรุณากรอกที่อยู่"}},
+        }
+
+    def validate_phone(self, value):
+        if len(value) != 10:
+            raise serializers.ValidationError(
+                "* กรุณากรอกเบอร์มือถือให้ถูกต้อง")
+        return value
--- API/views.py
+++ API/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from API.serializers import stockSerializer, supplierSerializer
+from API.serializers import stockSerializer, supplierSerializer, customerSerializer
 from Manage.models import Stock, Supplier, Customer, Stock
 from Account.models import Employee
 
@@ -99,6 +99,16 @@ class api_employee(APIView):
 
 class api_customer(APIView):
     permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        data = json.loads(request.body)
+        instance = Customer.objects.get(pk=data.get('id'))
+        serializer = customerSerializer(
+            data=data, instance=instance, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
         data = json.loads(request.body)
