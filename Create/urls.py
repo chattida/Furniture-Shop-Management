b2cb7a67f820c1a6c49419c111dd1fa8dec4478f
napat <napatkrup@hotmail.com>
2020-04-27 14:06:09 +0700

    :rocket: Update create order

--- Create/urls.py
+++ Create/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('create_order/', views.create_order, name='create_order'),
-    path('send_order/', views.send_order, name='send_order')
+    path('send_order/', views.send_order, name='send_order'),
+    path('show_status/', views.show_status, name='show_status')
 ]
